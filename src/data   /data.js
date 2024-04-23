2ac69b7429477ff2356bfc602c1e3c20ae7f6cf8
Rakavi Radhakrishnan <153973093+RakaviRadhakrishnan@users.noreply.github.com>
2024-04-23 19:00:48 +0530

    Create data.js

---
+++ src/data   /data.js
@@ -0,0 +1,72 @@
+export const data = [
+    {
+    ID : "1",
+    Name: "Raja",
+    Batch: "B-43 Tamil",
+    Email: "rajakumar@gmail.com",
+    Experience: 1,
+},
+{
+    ID : "2",
+    Name: "Arun",
+    Batch: "B-43 Tamil",
+    Email: "arunkumar@gmail.com",
+    Experience: 2,
+},
+{
+    ID : "3",
+    Name: "Sam",
+    Batch: " B-43 Tamil",
+    Email: "sam12@gmail.com",
+    Experience: 3,
+},
+{
+    ID : "4",
+    Name: "Parba",
+    Batch: "B-43 Tamil",
+    Email: "Praba32@gmail.com",
+    Experience: 2,
+},
+{
+    ID : "5",
+    Name: "Selva jee",
+    Batch: "B-43 Tamil",
+    Email: "selva21@gmail.com",
+    Experience: 2,
+},
+{
+    ID : "6",
+    Name: "Jhon",
+    Batch: "B-43 Tamil",
+    Email: "john65@gmail.com",
+    Experience: 2,
+},
+{
+    ID : "7",
+    Name: "Vishnu",
+    Batch: "B-43 Tamil",
+    Email: "visu123@gmail.com",
+    Experience: 1.5,
+},
+{
+    ID : "8",
+    Name: "Kumar",
+    Batch: "B-43 Tamil",
+    Email: "kumar5@gmail.com",
+    Experience: 1,
+},
+{
+    ID : "9",
+    Name: "Sugan",
+    Batch: "B-43 Tamil",
+    Email: "sugan35@gmail.com",
+    Experience: 2,
+},
+{
+    ID : "10",
+    Name: "Rajesh",
+    Batch: "B-43 Tamil",
+    Email: "rajesh85@gmail.com",
+    Experience: 1,
+}
+]
